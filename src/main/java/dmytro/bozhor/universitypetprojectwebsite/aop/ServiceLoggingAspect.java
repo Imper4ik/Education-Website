a8272c58be459fab4cc6ce83f7c2377b1a8263a0
DmytroBozhor <dimaastop1@gmail.com>
2024-04-13 16:41:44 +0200

    logging system across controllers and services added

---
+++ src/main/java/dmytro/bozhor/universitypetprojectwebsite/aop/ServiceLoggingAspect.java
@@ -0,0 +1,36 @@
+package dmytro.bozhor.universitypetprojectwebsite.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ServiceLoggingAspect extends AbstractLoggingAspect {
+
+    @Pointcut(value = "execution(public * *(..))")
+    private void anyPublicMethod() {
+    }
+
+    @Pointcut(value = "within(dmytro.bozhor.universitypetprojectwebsite.service..*)")
+    private void withinPath() {
+    }
+
+    @Override
+    @Before("anyPublicMethod() && withinPath()")
+    public void logBeforeAdvice(JoinPoint joinPoint) {
+        super.logBeforeAdvice(joinPoint);
+    }
+
+    @Override
+    @AfterReturning("anyPublicMethod() && withinPath()")
+    public void logAfterReturningAdvice(JoinPoint joinPoint) {
+        super.logAfterReturningAdvice(joinPoint);
+    }
+
+    @Override
+    @AfterThrowing(value = "anyPublicMethod() && withinPath()", throwing = "throwable")
+    public void logAfterThrowingAdvice(JoinPoint joinPoint, Throwable throwable) {
+        super.logAfterThrowingAdvice(joinPoint, throwable);
+    }
+}
